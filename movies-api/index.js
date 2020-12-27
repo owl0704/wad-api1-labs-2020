22ff12b8f2df092f45e869883de0555c911ce001
lizhixiang <3045055246@qq.com>
2020-12-27 22:13:48 +0700

    Week 9 Included Mongoose

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,10 +2,15 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
+import './db';
+import {loadUsers} from './seedData'
 dotenv.config();
 const errHandler = (err, req, res, next) => {
     /* if the error in development then send stack trace to display whole error,
     if it's in production then just send error message  */
+    if (process.env.SEED_DB) {
+        loadUsers();
+      }
     if(process.env.NODE_ENV === 'production') {
       return res.status(500).send(`Something went wrong!`);
     }
