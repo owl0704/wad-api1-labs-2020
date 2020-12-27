6b1769807e35542fcde2723292c2dcdd24efe129
lizhixiang <3045055246@qq.com>
2020-12-27 17:32:18 +0700

    Initial Express App

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,16 +1,21 @@
-import dotenv from 'dotenv';
 import express from 'express';
-import moviesRouter from './api/movies';
+import {moviesObject} from './movies';
 
-dotenv.config();
+const router = express.Router(); // eslint-disable-line
 
-const app = express();
-
-const port = process.env.PORT;
-
-app.use(express.static('public'));
-app.use('/api/movies', moviesRouter);
-
-app.listen(port, () => {
-  console.info(`Server running at ${port}`);
+router.get('/', (req, res) => {
+  res.status(200).send(moviesObject);
 });
+//get a movie
+router.get('/:id', (req, res) => {
+    const key =  parseInt(req.params.id);
+    const index = moviesObject.movies.map((movie)=>{
+  return movie.id;
+  }).indexOf(key);
+   if (index > -1) {
+       res.status(200).send(moviesObject.movies[index]);
+   } else {
+     res.status(404).send({message: `Unable to find movie with id: ${key}.`, status: 404});
+     }
+  });
+export default router;
