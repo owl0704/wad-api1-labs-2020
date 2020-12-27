7f5839e4228b462e391ffd7b2c21ffb56b7b978f
lizhixiang <3045055246@qq.com>
2020-12-27 23:15:32 +0700

    Week 9 Exercises

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -3,14 +3,14 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const MovieSchema = new Schema({
-    id: Number,
-    title: String
+    id: {type:Number, required:true},
+  title: {type:String, required:true}
   });
   
   const UserSchema = new Schema({
     username: { type: String, unique: true, required: true},
     password: {type: String, required: true },
-    favourites: [MovieSchema]
+    favourites: [MovieSchema,]
   });
   
 
--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
+import genresRouter from './api/genres';
 import './db';
 import {loadUsers} from './seedData'
 import usersRouter from './api/users';
@@ -24,6 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
+app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
 app.use(errHandler);
 app.listen(port, () => {
