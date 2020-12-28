920b656652b4d594e5683592154583e3f532ce8f
lizhixiang <3045055246@qq.com>
2020-12-29 01:14:19 +0700

    Topic 10 - Session based Auth

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,30 +4,37 @@ import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 import genresRouter from './api/genres';
 import './db';
-import {loadUsers} from './seedData'
+import { loadUsers } from './seedData'
 import usersRouter from './api/users';
+import session from 'express-session';
+import authenticate from './authenticate';
 dotenv.config();
 const errHandler = (err, req, res, next) => {
     /* if the error in development then send stack trace to display whole error,
     if it's in production then just send error message  */
     if (process.env.SEED_DB) {
         loadUsers();
-      }
-    if(process.env.NODE_ENV === 'production') {
-      return res.status(500).send(`Something went wrong!`);
+    }
+    if (process.env.NODE_ENV === 'production') {
+        return res.status(500).send(`Something went wrong!`);
     }
     res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
-  };
+};
 const app = express();
 
 const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
-app.use('/api/movies', moviesRouter);
+app.use(session({
+    secret: 'ilikecake',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use('/api/movies', authenticate, moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
 app.use(errHandler);
 app.listen(port, () => {
-  console.info(`Server running at ${port}`);
+    console.info(`Server running at ${port}`);
 });
