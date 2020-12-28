059fab1dafacc7a787229e2e8d7c2d1f33e6ed6c
lizhixiang <3045055246@qq.com>
2020-12-29 02:24:16 +0700

    Topic 10 - Update Movies Data/Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,17 +4,19 @@ import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 import genresRouter from './api/genres';
 import './db';
-import { loadUsers } from './seedData'
+import {loadUsers, loadMovies} from './seedData';
 import usersRouter from './api/users';
 import session from 'express-session';
 import passport from './authenticate';
+
 dotenv.config();
 const errHandler = (err, req, res, next) => {
     /* if the error in development then send stack trace to display whole error,
     if it's in production then just send error message  */
     if (process.env.SEED_DB) {
         loadUsers();
-    }
+        loadMovies();
+      }
     if (process.env.NODE_ENV === 'production') {
         return res.status(500).send(`Something went wrong!`);
     }
